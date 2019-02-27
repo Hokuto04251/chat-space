82f26b4fb829f133bd3ded86407832357ccd49b0
Hokuto04251 <sh.79-ac5u@keio.jp>
2019-02-27 22:15:44 +0900

    [修正済み]メッセージ送信機能まで

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,16 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-<<<<<<< Updated upstream
   end
 
-=======
 
-
-  end
-
->>>>>>> Stashed changes
-  def create
+ def create
     @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
@@ -21,26 +15,18 @@ class MessagesController < ApplicationController
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
-<<<<<<< Updated upstream
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
-
-=======
     end
   end
 
+
+
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
->>>>>>> Stashed changes
+
   def set_group
     @group = Group.find(params[:group_id])
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,7 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
-<<<<<<< Updated upstream
+
   mount_uploader :image, ImageUploader
-=======
-  mount_uploader :image,ImageUploader
->>>>>>> Stashed changes
+
 end
